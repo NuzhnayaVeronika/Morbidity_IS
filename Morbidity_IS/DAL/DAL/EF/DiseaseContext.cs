3be6d49fb088e588b343085ee19796ffb89d6f3e
Veronika Nuzhnaya <nika.nuzhna@gmail.com>
2020-06-17 11:23:39 +0300

    z

---
+++ Morbidity_IS/DAL/DAL/EF/DiseaseContext.cs
@@ -0,0 +1,20 @@
+﻿using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.EF
+{
+    public class DiseaseContext
+        : DbContext
+    {
+        internal DbSet<Disease> Diseases { get; set; }
+
+        public DiseaseContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+    }
+}
--- Morbidity_IS/DAL/DAL/Entities/Disease.cs
+++ Morbidity_IS/DAL/DAL/Entities/Disease.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace DataFilling.DAL.Entities
+namespace DAL.Entities
 {
     class Disease
     {
