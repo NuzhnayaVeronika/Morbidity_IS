efabccafd4d06ae33ffb39cf8d22003396b05635
Veronika Nuzhnaya <nika.nuzhna@gmail.com>
2020-06-17 10:11:18 +0300

    Added Entities to DAL

---
+++ Morbidity_IS/Morbidity_IS/DataFilling/DAL/DAL/Entities/Disease.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataFilling.DAL.Entities
+{
+    class Disease
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
